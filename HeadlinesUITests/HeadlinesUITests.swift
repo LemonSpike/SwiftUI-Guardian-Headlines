380bf257e100bf5b1f5eae199644d7dfcb5ea67e
Pranav Kasetti <pranavkasetti@gmail.com>
2021-08-27 17:52:33 +0100

    Add placeholder for stubbed UI tests.

--- HeadlinesUITests/HeadlinesUITests.swift
+++ HeadlinesUITests/HeadlinesUITests.swift
@@ -6,20 +6,25 @@
 //
 
 import XCTest
+import OHHTTPStubs
+import OHHTTPStubsSwift
+@testable import Headlines
 
 class HeadlinesUITests: XCTestCase {
 
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
+    stub(condition: isHost("content.guardianapis.com")) { request in
+      return HTTPStubsResponse(
+        fileAtPath: OHPathForFile("headline_stub.json", type(of: self))!,
+        statusCode: 200,
+        headers: ["Content-Type":"application/json"]
+      )
+    }
     continueAfterFailure = false
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
   }
 
   override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    HTTPStubs.removeAllStubs()
   }
 
   func testExample() throws {
@@ -33,7 +38,6 @@ class HeadlinesUITests: XCTestCase {
 
   func testLaunchPerformance() throws {
     if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-      // This measures how long it takes to launch your application.
       measure(metrics: [XCTApplicationLaunchMetric()]) {
         XCUIApplication().launch()
       }
