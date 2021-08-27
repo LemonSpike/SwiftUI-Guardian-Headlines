252f347a3fa9b4e9a6421c5e81f30481efed63c7
Pranav Kasetti <pranavkasetti@gmail.com>
2021-08-27 08:58:44 +0100

    Project setup :tada:

---
+++ Headlines/HeadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlinesApp.swift
+//  Headlines
+//
+//  Created by Pranav Kasetti on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct HeadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
