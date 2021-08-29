860af9a7d893703a7d08c6f28bc116ea61c44835
Pranav Kasetti <pranavkasetti@gmail.com>
2021-08-29 01:40:50 +0100

    Add Services convenience init.

---
+++ Headlines/Services/Services+Init.swift
@@ -0,0 +1,16 @@
+//
+//  Services+Init.swift
+//  Headlines
+//
+//  Created by Pranav Kasetti on 28/08/2021.
+//
+
+import Foundation
+
+extension HeadlineServices {
+  convenience init() {
+    self.init(networkService: URLSession.shared,
+              storageService: ArticleStorageService(),
+              displayingService: .init())
+  }
+}
