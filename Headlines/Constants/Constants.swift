1378b10aed181da7af7ab6b6cc836067643f6d30
Pranav Kasetti <pranavkasetti@gmail.com>
2021-08-29 01:32:04 +0100

    Add Constants and URLs.

---
+++ Headlines/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Headlines
+//
+// Created by Pranav Kasetti on 28/08/2021.
+//
+
+import Foundation
+
+struct Constants {
+  // Please contact me for the valid API Key.
+  static let APIKey = ""
+}
---
+++ Headlines/Constants/URLs.swift
@@ -0,0 +1,16 @@
+//
+//  URLs.swift
+//  Headlines
+//
+// Created by Pranav Kasetti on 28/08/2021.
+//
+
+import Foundation
+
+struct URLs {
+  static let articleBaseURL = "https://content.guardianapis.com"
+  static let articleSearchPath = "/search"
+  static let articleShowFieldsParam = "show-fields"
+  static let articleQueryParam = "q"
+  static let articleAPIKeyParam = "api-key"
+}
