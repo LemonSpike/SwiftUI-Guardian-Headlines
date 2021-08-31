2e952874c59cb4aaa5bafaf74adc4ca33e708153
Pranav Kasetti <pranavkasetti@gmail.com>
2021-09-01 00:01:32 +0100

    Add favorites view UI tests.

--- HeadlinesUITests/HeadlinesUITests.swift
+++ HeadlinesUITests/HeadlinesUITests.swift
@@ -147,6 +147,49 @@ Rishi Sunak to announce £15bn green finance plan
     }
   }
 
+  func testFavouriteArticlesInList() {
+    let app = XCUIApplication(bundleIdentifier: "com.kasprasolutions.Headlines")
+    app.launch()
+
+    let alert = app.alerts["Welcome to Headlines 🗞"]
+    XCTAssert(alert
+                .waitForExistence(timeout: 5))
+    alert.buttons["Ok"].tap()
+
+    XCTAssert(app.images["favorite"].waitForExistence(timeout: 5))
+    if !app.images["articleStarIcon"].exists {
+      XCTAssert(app.images["articleStarFillIcon"].waitForExistence(timeout: 5))
+      app.images["articleStarFillIcon"].tap()
+      XCTAssert(app.images["articleStarIcon"].waitForExistence(timeout: 5))
+      app.images["articleStarIcon"].tap()
+      XCTAssert(app.images["articleStarFillIcon"].waitForExistence(timeout: 5))
+    } else {
+      XCTAssert(app.images["articleStarIcon"].waitForExistence(timeout: 5))
+      app.images["articleStarIcon"].tap()
+      XCTAssert(app.images["articleStarFillIcon"].waitForExistence(timeout: 5))
+      app.images["articleStarFillIcon"].tap()
+      XCTAssert(app.images["articleStarIcon"].waitForExistence(timeout: 5))
+    }
+
+    app.buttons["Favourites"].tap()
+    XCTAssert(app
+                .staticTexts["Rishi Sunak to announce £15bn green finance plan"]
+                .waitForExistence(timeout: 5))
+    XCTAssert(app
+                .staticTexts["1 Jul 2021"]
+                .waitForExistence(timeout: 5))
+    XCTAssert(app
+                .staticTexts["1 Favourite Articles"]
+                .waitForExistence(timeout: 5))
+
+    XCTAssert(app.images["articleStarFillIcon"].waitForExistence(timeout: 5))
+    app.images["articleStarFillIcon"].tap()
+
+    XCTAssert(app
+                .staticTexts["0 Favourite Articles"]
+                .waitForExistence(timeout: 5))
+  }
+
   func testLaunchPerformance() throws {
     if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
       measure(metrics: [XCTApplicationLaunchMetric()]) {
